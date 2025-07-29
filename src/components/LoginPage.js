c89fd606010c4d2cf5433ef2e7e189d7f2f905bb
VinodPujari7019 <vinodrpujari@gmail.com>
2025-07-29 11:12:59 +0530

    firstpage

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";  
+
+const LoginPage = () => {
+    return (
+        <div className="login">
+            <h1> Welcome to my first project</h1>
+        </div>
+    )
+
+}
+
+export default LoginPage;
